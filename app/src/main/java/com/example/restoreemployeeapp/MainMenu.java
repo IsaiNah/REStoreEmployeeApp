c00fade98394d35c1574fad9f338c1035e0c8c3b
IsaiNah <38513474+IsaiNah@users.noreply.github.com>
2019-04-09 19:53:00 -0400

    - Button to call number now working

--- app/src/main/java/com/example/restoreemployeeapp/MainMenu.java
+++ app/src/main/java/com/example/restoreemployeeapp/MainMenu.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,8 +18,7 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        callIntent = new Intent (Intent.ACTION_CALL);
-        callIntent.setData(Uri.parse("1234567890"));
+
 
 
         btnCallManager = (Button) findViewById(R.id.btn_menu_callmanager);
@@ -27,7 +27,14 @@ public class MainMenu extends AppCompatActivity {
         btnCallManager.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(callIntent);
+                try{
+                    callIntent = new Intent (Intent.ACTION_DIAL);
+                    callIntent.setData(Uri.parse("tel:123456789"));
+                    startActivity(callIntent);
+                }catch (Exception e)
+                {
+                    Log.d(TAG, "onClick: Exception " + e);
+                }
             }
         });
 
