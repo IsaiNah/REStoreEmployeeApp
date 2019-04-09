4f832c38d32ab3c787863280987036fd32f1eeda
IsaiNah <38513474+IsaiNah@users.noreply.github.com>
2019-04-09 19:39:59 -0400

    -Added MainMenu Activity to manifest - ClickListener

--- app/src/main/java/com/example/restoreemployeeapp/MainMenu.java
+++ app/src/main/java/com/example/restoreemployeeapp/MainMenu.java
@@ -2,14 +2,27 @@ package com.example.restoreemployeeapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
     private static final String TAG = "SEA_Log";
+    private Button btnCallManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        btnCallManager = (Button) findViewById(R.id.btn_menu_callmanager);
+
+
+        btnCallManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
     }
 }
