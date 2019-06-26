7b5327bdab89eed93d8d9ed51933323519bad9e0
IsaiNah <38513474+IsaiNah@users.noreply.github.com>
2019-06-26 00:45:51 -0400

    - Created Drag and Drop demo in order to create scheduler - Major change to way schedulaing will happen

--- app/src/main/java/com/example/restoreemployeeapp/UsersData.java
+++ app/src/main/java/com/example/restoreemployeeapp/UsersData.java
@@ -11,29 +11,29 @@ public class UsersData {
 
     private List<Employee> usersList = new ArrayList<Employee>() {
         {
-            add(new Employee(0, "", "", "Developers"));
-            add(new Employee(1, "Frederick Hoffman", "https://randomEmployee.me/api/portraits/men/52.jpg", "Developers"));
-            add(new Employee(2, "Calvin Young", "https://randomEmployee.me/api/portraits/men/78.jpg", "Developers"));
-            add(new Employee(3, "Jeanette Reid", "https://randomEmployee.me/api/portraits/women/37.jpg", "Developers"));
-            add(new Employee(4, "Flenn Wilson", "https://randomEmployee.me/api/portraits/men/40.jpg", "Developers"));
-            add(new Employee(5, "Martin Holland", "https://randomEmployee.me/api/portraits/men/0.jpg", "Developers"));
-            add(new Employee(6, "", "", "Designers"));
-            add(new Employee(7, "Jeanette Simmmons", "https://randomEmployee.me/api/portraits/women/3.jpg", "Designers"));
-            add(new Employee(8, "Wallace Lambert", "https://randomEmployee.me/api/portraits/men/53.jpg", "Designers"));
-            add(new Employee(9, "Andy Clark", "https://randomEmployee.me/api/portraits/men/68.jpg", "Designers"));
-            add(new Employee(10, "olivia obrien", "https://randomEmployee.me/api/portraits/women/93.jpg", "Designers"));
-            add(new Employee(11, "Debbie Bennett", "https://randomEmployee.me/api/portraits/women/34.jpg", "Designers"));
-            add(new Employee(12, "", "", "Team Leads"));
-            add(new Employee(13, "Bernice Lawson", "https://randomEmployee.me/api/portraits/women/20.jpg", "Team Leads"));
-            add(new Employee(14, "Camila Elliott", "https://randomEmployee.me/api/portraits/women/60.jpg", "Team Leads"));
-            add(new Employee(15, "Gerald Webb", "https://randomEmployee.me/api/portraits/men/55.jpg", "Team Leads"));
-            add(new Employee(16, "Russell Hart", "https://randomEmployee.me/api/portraits/men/18.jpg", "Team Leads"));
-            add(new Employee(17, "Joyce Mccoy", "https://randomEmployee.me/api/portraits/women/82.jpg", "Team Leads"));
-            add(new Employee(18, "", "", "Team Managers"));
-            add(new Employee(19, "Daryl Banks", "https://randomEmployee.me/api/portraits/men/4.jpg", "Team Managers"));
-            add(new Employee(20, "Veronica Vargas", "https://randomEmployee.me/api/portraits/women/14.jpg", "Team Managers"));
-            add(new Employee(21, "Natalie Jacobs", "https://randomEmployee.me/api/portraits/women/0.jpg", "Team Managers"));
-            add(new Employee(22, "Beverly Kennedy", "https://randomEmployee.me/api/portraits/women/30.jpg", "Team Managers"));
+            add(new Employee(0, "", "", "OroGold Barrie"));
+            add(new Employee(1, "", "", "OroGold Scarborough"));
+            add(new Employee(2, "", "", "OroGold Brampton"));
+            add(new Employee(3, "", "", "Not Set"));
+            add(new Employee(4, "Frederick Hoffman", "https://randomUser.me/api/portraits/men/52.jpg", "Not Set"));
+            add(new Employee(5, "Calvin Young", "https://randomUser.me/api/portraits/men/78.jpg", "Not Set"));
+            add(new Employee(6, "Jeanette Reid", "https://randomUser.me/api/portraits/women/37.jpg", "Not Set"));
+            add(new Employee(7, "Flenn Wilson", "https://randomUser.me/api/portraits/men/40.jpg", "Not Set"));
+            add(new Employee(8, "Martin Holland", "https://randomUser.me/api/portraits/men/0.jpg", "Not Set"));
+            add(new Employee(9, "Jeanette Simmmons", "https://randomUser.me/api/portraits/women/3.jpg", "Not Set"));
+            add(new Employee(10, "Wallace Lambert", "https://randomUser.me/api/portraits/men/53.jpg", "Not Set"));
+            add(new Employee(11, "Andy Clark", "https://randomUser.me/api/portraits/men/68.jpg", "Not Set"));
+            add(new Employee(12, "olivia obrien", "https://randomUser.me/api/portraits/women/93.jpg", "Not Set"));
+            add(new Employee(13, "Debbie Bennett", "https://randomUser.me/api/portraits/women/34.jpg", "Not Set"));
+            add(new Employee(14, "Bernice Lawson", "https://randomUser.me/api/portraits/women/20.jpg", "Not Set"));
+            add(new Employee(15, "Camila Elliott", "https://randomUser.me/api/portraits/women/60.jpg", "Not Set"));
+            add(new Employee(16, "Gerald Webb", "https://randomUser.me/api/portraits/men/55.jpg", "Not Set"));
+            add(new Employee(17, "Russell Hart", "https://randomUser.me/api/portraits/men/18.jpg", "Not Set"));
+            add(new Employee(18, "Joyce Mccoy", "https://randomUser.me/api/portraits/women/82.jpg", "Not Set"));
+            add(new Employee(19, "Daryl Banks", "https://randomUser.me/api/portraits/men/4.jpg", "Not Sets"));
+            add(new Employee(20, "Veronica Vargas", "https://randomUser.me/api/portraits/women/14.jpg", "Not Set"));
+            add(new Employee(21, "Natalie Jacobs", "https://randomUser.me/api/portraits/women/0.jpg", "Not Set"));
+            add(new Employee(22, "Beverly Kennedy", "https://randomUser.me/api/portraits/women/30.jpg", "Not Set"));
         }
     };
 
